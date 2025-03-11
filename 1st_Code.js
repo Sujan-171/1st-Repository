b00ebb163894e9e8338618591ff3f16bf421e29d
Sujan Sarker <ssarker.cse@gmail.com>
2025-03-11 13:17:46 +0600

    summation pratice with JS

---
+++ 1st_Code.js
@@ -0,0 +1,10 @@
+// Function to calculate the sum of two numbers
+function sum(a, b) {
+    return a + b;
+}
+
+let num1 = 5;
+let num2 = 10;
+let result = sum(num1, num2);
+
+console.log("The sum is: " + result);
